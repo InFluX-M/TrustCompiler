5a76d4042dbe67783650d89ec107b1d545367bb8
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-07-08 21:52:33 +0330

    use code generator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "LexicalAnalyzer/lexical_analyzer.h"
 #include "SyntaxAnalyzer/syntax_analyzer.h"
 #include "SemanticAnalyzer/semantic_analyzer.h"
+#include "CodeGenerator/code_generator.h"
 #include <filesystem>
 
 #include <iostream>
@@ -19,6 +20,11 @@ int main() {
 
     SemanticAnalyzer sem_analyzer(syn_analyzer.get_tree().get_root(), output_file + file + ".sem");
     sem_analyzer.analyze();
+
+    CodeGenerator code_generator(syn_analyzer.get_tree().get_root(), sem_analyzer.get_symbol_table(),
+                                 output_file + file + ".c");
+
+    code_generator.run();
 
     return SUCCESS;
 }
