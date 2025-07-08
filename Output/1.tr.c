9ec82a1a0c17a8093a1f17ab2549e10a067d2157
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-07-09 00:03:43 +0330

    test 2 3

---
+++ Output/1.tr.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void main() {
+	int* x;
+	void y = 1;
+	const void c = y;
+	const void a = y+42;
+	int rr;
+x}
+
---
+++ Output/3.tr.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void main() {
+	const int a = 5;
+	const int b = 3;
+	const int sum = a+b;
+	const int product = a*b;
+	const int division = a/b;
+	const int mod_result = a%b;
+	printf("%d\n", sum);
+	printf("%d\n", product);
+	printf("%d\n", division);
+	printf("%d\n", mod_result);
+}
+
